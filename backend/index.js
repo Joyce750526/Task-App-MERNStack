0168c8dea46aeaa1e3bfad1825719fde0c8667a9
Joyce Chen <joyceideas@outlook.com>
2023-11-02 17:34:00 -0400

    created gitignore

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+// Model
+const taskSchema = require("./model/Task");
+const TaskAPP = mongoose.model("TaskApp", taskSchema);
+
 const app = express();
 console.log(app);
 const port = 4000;
 
 // Need to fix MongoDB Issue!!
 const MongoDBAccess =
-  "mongodb+srv://chaoying750526:<Joyce750526>@cluster0.f8lkifx.mongodb.net/";
+  "mongodb+srv://Apple750526:Apple750526@cluster0.f8lkifx.mongodb.net/";
 mongoose
   .connect(MongoDBAccess, { useNewUrlParser: true })
   .then(() => {
@@ -24,3 +28,31 @@ app.listen(port, () => {
 app.get("/", (req, res) => {
   res.send("hello");
 });
+
+// C R U D Operations
+
+// `Create` A NEW TASK
+let schoolTask = new TaskAPP({
+  name: "read a book",
+  date: "26 Oct 2023",
+  isDone: false,
+});
+schoolTask.save();
+
+// HTTP Methods
+// Post, Get, Put, Patch, Delete
+
+app. post ("./task", (req,res)=>{
+  let schoolTask=new TaskAPP({
+    name: "read a book",
+    date: "26 Oct 2023",
+    isDone: false,
+  })
+  let data = await schoolTask.save()
+  res.send(data)
+})
+
+
+// `Read`
+// `Update`
+// `Delete`
